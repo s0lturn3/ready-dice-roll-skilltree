27e5c461cd13b8f76d5303499a272d1390a85a60
Erick Carvalho <erickzcarvalho25@outlook.com>
2025-10-02 18:20:01 -0300

    init: inicialização do repositório e projeto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { routes } from './app.routes';
+import { CustomPreset } from './custom-preset';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    // PrimeNG
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: CustomPreset
+      },
+      ripple: true
+    }),
+
+    MessageService,
+    ConfirmationService
+  ]
 };
